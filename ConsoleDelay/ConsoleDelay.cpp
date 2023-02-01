38d7ad8bff1ca07ff9cf0b4d5f87d34a0bcb3d0f
root <root@v1644357.hosted-by-vdsina.ru>
2023-02-01 11:18:41 +0300

    2.0

--- ConsoleDelay/ConsoleDelay.cpp
+++ ConsoleDelay/ConsoleDelay.cpp
@@ -1,12 +1,13 @@
-﻿#include <iostream>
+﻿#include <cstddef>
+#include <iostream>
 using namespace std;
 
 /// Сбросить флаги ошибок и содержимое буфера потока.
 void ignore_all(istream& is)
 {
-    is.clear(); // Сброс ошибок.
-    is.sync();  // Синхронизация объекта потока с внешним устройством.
-    is.ignore(is.rdbuf()->in_avail()); // Сброс символов уже считанных в буфер потока.
+    is.clear();
+    is.sync();
+    is.ignore(is.rdbuf()->in_avail());
 }
 
 /// Задержка экрана = сброс стандартного потока ввода и ожидание следующего символа.
@@ -25,6 +26,10 @@ int main()
         cout << "Enter a sequence of integers:\n";
         for (int i; cin >> i;)
             cout << i << ' ';
+
+        // Выйти, если введён признак конца файла.
+        if (cin.eof())
+            return EXIT_SUCCESS;
 
         cout << "\nPress Enter to repeat\n";
         console_delay();
